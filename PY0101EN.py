8ef23759eb6b4183703729b1dc61c8ce7f3c50b8
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2020-07-20 23:49:32 +0200

    Update PY0101EN.py

--- PY0101EN.py
+++ PY0101EN.py
@@ -1,8 +1,17 @@
 A = np.array([[0, 1, 1], [1, 0, 1]])
 B = np.array([[1, 1], [1, 1], [-1, 1]])
-Z = np.dot(A,B)   # results in [[0, 2], [0, 2]])
-np.sin(Z)   # results in [[0.        , 0.90929743],   [0.        , 0.90929743]]
-C = np.array([[1,1],[2,2],[3,3]])   # calculate the transposed matrix
-C   # results in [[1, 1],        [2, 2],       [3, 3]]
-C.T   # results in [[1, 2, 3],     [1, 2, 3]]
+
+Z = np.dot(A,B)
+# results in [[0, 2], [0, 2]])
+
+np.sin(Z)
+# results in [[0.        , 0.90929743],   [0.        , 0.90929743]]
+
+C = np.array([[1,1],[2,2],[3,3]])
+# calculate the transposed matrix
+C
+# results in [[1, 1],        [2, 2],       [3, 3]]
+
+C.T
+# results in [[1, 2, 3],     [1, 2, 3]]
 
